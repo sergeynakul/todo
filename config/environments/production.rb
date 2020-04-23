afbdfe167660f065575af5f74bf4b5490b0b3ca4
Sergio <nakul.sv@gmail.com>
2020-04-23 20:48:41 +0300

    sendgrid for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,18 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "todo_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'blooming-atoll-27654.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'blooming-atoll-27654.herokuapp.com',
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: "https://blooming-atoll-27654.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
